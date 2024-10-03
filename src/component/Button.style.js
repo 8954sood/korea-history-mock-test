c0d4d20ad78f2b1ebc7bf3ccd5a305d247697bb8
8954sood <8954sood@naver.com>
2024-10-04 01:07:48 +0900

    feat: Create Button Component

---
+++ src/component/Button.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    width: calc(100% - 40px);
+    height: 56px;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    background-color: #0083F0;
+    border-radius: 12px;
+    border-width: 0px;
+    text-align: center;
+    font-family: "KBIZ_sgo";
+    font-size: 16px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
