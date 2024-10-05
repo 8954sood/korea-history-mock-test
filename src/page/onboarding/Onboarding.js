db61299d72e88e249e4d7e1f2fea8ddc376cbaad
8954sood <8954sood@naver.com>
2024-10-05 15:02:34 +0900

    chore: Edit OnBoarding Text

--- src/page/onboarding/Onboarding.js
+++ src/page/onboarding/Onboarding.js
@@ -17,7 +17,7 @@ const Onboarding = () => {
             {page == 0? "2024 2학년 2학기 중간고사 \n(모의) 한국사": "유의사항"}
         </S.Title>
         <S.FirstContent>
-            {page == 0? "열심히 준비한 한국사 실력을\n검증해보세요!": "총 문제는 ?개 이며, 실제 시험 문제와\n매우 다를 수 있음을 알립니다."}
+            {page == 0? "열심히 준비한 한국사 실력을\n검증해보세요!": "총 문제는 10개 이며, 실제 시험 문제와\n매우 다를 수 있음을 알립니다."}
         </S.FirstContent>
         <Spacer height="40px"/>
         <Button onClick={() => {
